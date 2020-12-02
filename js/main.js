8736d3f85385cdd981c79e18c8b1496a9335b08a
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2020-12-02 08:27:55 -0600

    Update functionality to OOP approach

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-let money = 0;
-let click_value = 1;
+class Game {
+  constructor() {
+    this.click = 1;
+    this.multiplier = 1;
+    this.money = new Money();
+
+  //   this.pickUpgrade = {
+  //     cost: 10,
+  //     upgrade: 1,
+  //   }
+
+  //   this.mineUpgrade = {
+  //     cost: 10,
+  //     upgrade: 3,
+  //   }
+  }
+
+  // update() {
+
+  // }
+
+}
+
+class Money {
+  constructor() {
+    this.money = 0;
+    this.page = document.getElementById("money");
+  }
+
+  update(value) {
+    this.money += value;
+    this.page.innerText = `${this.money.toFixed(0)}`;
+  }
+}
+
+const game = new Game();
 
 const clicker = document.getElementById("clicker");
-const html_money = document.getElementById("money");
 
-clicker.addEventListener("click", function(e){
-  money += click_value;
-  html_money.innerText = `${money}`;
+clicker.addEventListener("click", function(){
+  game.money.update(game.click * game.multiplier);
 });
