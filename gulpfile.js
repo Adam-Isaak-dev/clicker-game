5b30a19ee935150a5b7e37853bef65c33d984d85
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2020-12-06 11:50:32 -0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,50 @@
 const { src, dest, series, parallel } =  require('gulp');
+const postcss = require ('gulp-postcss');
+const cssnano = require ('cssnano');
+const autoprefixer = require ('autoprefixer'); 
+const sourcemaps = require ('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const terser = require('gulp-terser');
+const clean = require('gulp-clean');
 
-// html task
+
+function cleanTask() {
+  return src('dist/*')
+    .pipe(clean())
+}
+
 function htmlTask() {
   return src('src/*.html')
-    .pipe(dest('dist'))
+    .pipe(dest('dist/'))
 }
 
-// scripts task
-function jsTask() {
-  return src('src/js/*.js')
-    .pipe(dest('dist/js'))
-}
-
-// scripts task
 function cssTask() {
   return src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer(), cssnano() ]))
+    .pipe(concat('all.css'))
+    .pipe(sourcemaps.write())
     .pipe(dest('dist/css'))
 }
 
+function jsTask() {
+  return src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/js'))
+}
+
+function imageTask() {
+  return src('src/images/*')
+  .pipe(imagemin())
+  .pipe(dest('dist/images'))
+}
+
 exports.html = htmlTask;
-exports.scripts = jsTask;
-exports.styles = cssTask;
-exports.default = series(htmlTask, parallel(jsTask, cssTask));
+exports.css = cssTask;
+exports.js = jsTask;
+exports.images = imageTask;
+exports.default = series(cleanTask, htmlTask, parallel(cssTask, jsTask, imageTask));
